dfb17ddd3bf3a0692ea8d8115bac8f82811a5f14
sathvikR25 <sathvikgth@gmail.com>
2025-01-03 09:57:23 +0530

    Add files via upload

---
+++ demo5.java
@@ -0,0 +1,16 @@
+
+import java.util.Scanner;
+
+public class Demo005 {
+
+    public static void main(String[] args) {
+        String username = "user001";
+        String password = "pwd_001";
+        while ((!username == "user001") && (password == "password001")) {
+
+            System.out.println("Happy New Year!");
+            break;
+        }
+
+    }
+}
