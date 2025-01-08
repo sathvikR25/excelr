ed2ab7995abed91f2487a5a95979b942520587e5
sathvikR25 <sathvikgth@gmail.com>
2025-01-08 10:19:41 +0530

    Add files via upload

---
+++ Demo18.java
@@ -0,0 +1,22 @@
+
+import java.util.Scanner;
+
+class Demo18 {
+
+    public static void main(String[] args) {
+
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.print("Enter the number: ");
+        int number = scanner.nextInt();
+
+        System.out.print("Enter the multiple to find: ");
+        int multiple = scanner.nextInt();
+
+        int nextMultiple = ((number / multiple) + 1) * multiple;
+
+        System.out.println("The next multiple of " + multiple + " after " + number + " is: " + nextMultiple);
+
+        scanner.close();
+    }
+}
