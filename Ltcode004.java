8e5cb469939f0bcf494e76601b77860f6da4dc01
sathvik r <sathvikgth@gmail.com>
2025-01-23 10:09:56 +0530

    Add files via upload

---
+++ Ltcode004.java
@@ -0,0 +1,55 @@
+class ltcode_004 {
+    public ListNode addTwoNumbers(ListNode l1, ListNode l2) {
+        ListNode dummy = new ListNode(0);
+        ListNode current = dummy;
+        int carry = 0;
+
+        while (l1 != null || l2 != null || carry != 0) {
+            int val1 = (l1 != null) ? l1.val : 0; 
+            int val2 = (l2 != null) ? l2.val : 0; 
+
+            int sum = val1 + val2 + carry;
+            carry = sum / 10;
+
+            current.next = new ListNode(sum % 10);
+            current = current.next;
+
+            if (l1 != null) l1 = l1.next;
+            if (l2 != null) l2 = l2.next;
+        }
+
+        return dummy.next;
+    }
+
+    public static void main(String[] args) {
+        // Create first list: 2 -> 4 -> 3 (represents 342)
+        ListNode l1 = new ListNode(2);
+        l1.next = new ListNode(4);
+        l1.next.next = new ListNode(3);
+
+        // Create second list: 5 -> 6 -> 4 (represents 465)
+        ListNode l2 = new ListNode(5);
+        l2.next = new ListNode(6);
+        l2.next.next = new ListNode(4);
+
+        // Create object of ltcode_004 and call addTwoNumbers
+        ltcode_004 solution = new ltcode_004();
+        ListNode result = solution.addTwoNumbers(l1, l2);
+
+        // Print the result list
+        while (result != null) {
+            System.out.print(result.val + " ");
+            result = result.next;
+        }
+        // Output should be: 7 0 8 (representing 807)
+    }
+}
+
+class ListNode {
+    int val;
+    ListNode next;
+    ListNode(int val) {
+        this.val = val;
+        this.next = null;
+    }
+}
