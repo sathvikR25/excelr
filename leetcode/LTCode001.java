342d2ac1e5e4d1f15c606c4fe3710728be869a3a
sathvik r <sathvikgth@gmail.com>
2025-01-27 09:56:07 +0530

    Add files via upload

--- leetcode/LTCode001.java
+++ leetcode/LTCode001.java
@@ -1,1 +1,29 @@
+public class LTCode_001 {
+    public static void main(String[] args) {
+        Solution solution = new Solution();
 
+        System.out.println(solution.isPalindrome(121));    
+        System.out.println(solution.isPalindrome(-121));   
+        System.out.println(solution.isPalindrome(10));     
+        System.out.println(solution.isPalindrome(0));      
+        System.out.println(solution.isPalindrome(12321));  
+    }
+}
+
+class Solution {
+    public boolean isPalindrome(int x) {
+        if (x < 0 || (x % 10 == 0 && x != 0)) {
+            return false;
+        }
+
+        int reversed = 0; 
+        int original = x; 
+        while (x > 0) {
+            int digit = x % 10;         
+            reversed = reversed * 10 + digit; 
+            x /= 10;                  
+        }
+
+        return original == reversed;
+    }
+}
